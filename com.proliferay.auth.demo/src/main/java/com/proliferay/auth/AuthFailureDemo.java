850d8d1eeb1a0eaac1b9dec5ee515c2fddffe9a0
hislam <hamidulasm@gmail.com>
2018-04-21 16:06:48 +0530

    added auth demo

---
+++ com.proliferay.auth.demo/src/main/java/com/proliferay/auth/AuthFailureDemo.java
@@ -0,0 +1,62 @@
+package com.proliferay.auth;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.AuthException;
+import com.liferay.portal.kernel.security.auth.AuthFailure;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+
+/**
+ * @author Hamidul Islam
+ */
+@Component(
+	immediate = true,
+	property = {
+	     "key=auth.failure"
+	},
+	service = AuthFailure.class
+)
+public class AuthFailureDemo implements AuthFailure {
+
+	@Override
+	public void onFailureByEmailAddress(long companyId, String emailAddress, Map<String, String[]> headerMap,
+			Map<String, String[]> parameterMap) throws AuthException {
+
+
+		
+		try{
+			
+			User user = UserLocalServiceUtil.getUserByEmailAddress(companyId, emailAddress);
+			
+			int failures = user.getFailedLoginAttempts();
+			
+			_log.info("Login Failed : Email Id :"+emailAddress);
+			_log.info("Login Attempts : "+failures); 
+		}catch (Exception e) {
+			_log.error("Exceptions while log in ",e);
+		}
+		
+	}
+
+	@Override
+	public void onFailureByScreenName(long companyId, String screenName, Map<String, String[]> headerMap,
+			Map<String, String[]> parameterMap) throws AuthException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFailureByUserId(long companyId, long userId, Map<String, String[]> headerMap,
+			Map<String, String[]> parameterMap) throws AuthException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(AuthFailureDemo.class);
+
+}
